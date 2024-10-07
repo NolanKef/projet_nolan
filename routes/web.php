b771af965321fbb7c7a7e0afe96a700442b004d0
NolanKef <nolan.keffala@gmail.com>
2024-10-07 11:10:39 +0200

    #

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hello', function () {
+    return 'Bonjour le monde !';
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
